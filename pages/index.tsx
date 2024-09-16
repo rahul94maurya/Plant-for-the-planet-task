11ac55106107bdfcaa91032b80732c0ee7f40b58
rahul <getrahulmaurya1994@gmail.com>
2024-09-16 23:45:20 +0530

    layout component is created

--- pages/index.tsx
+++ pages/index.tsx
@@ -149,7 +149,7 @@ export default function Home() {
             </div>
             <div className="flex items-center justify-between w-full px-4 py-4 sm:px-6 lg:px-8">
               <Link
-                href="/profile"
+                href="/edit-profile"
                 className="text-xl font-bold tracking-tight text-gray-900"
               >
                 Profile
