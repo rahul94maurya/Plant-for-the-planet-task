2b2072797dbce7acdc38c6e65d03402bf34f36fe
rahul <getrahulmaurya1994@gmail.com>
2024-09-16 16:56:40 +0530

    username and password fields are dynamic now

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,35 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link';
+import React, { use, useState } from 'react';
+import { authenticateUser } from '../utils/api';
 
 const LoginPage = () => {
+  const [userName, setUserName] = useState('emilys');
+  const [password, setPassword] = useState('emilyspass');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleUserNameChange = function (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+    setUserName(event.target.value);
+  };
+
+  const handlePasswordChange = function (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+    setPassword(event.target.value);
+  };
+
+  const handleFormSubmit = async function (
+    event: React.FormEvent<HTMLFormElement>
+  ) {
+    const requestBody = { username: 'emilys', password: 'emilyspass' };
+    event.preventDefault();
+    setIsLoading(true);
+    const response = await authenticateUser(requestBody);
+    setIsLoading(false);
+    console.log('response data from API', response);
+  };
+
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -11,12 +39,12 @@ const LoginPage = () => {
           className="mx-auto h-10 w-auto"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
+          Login to your account
         </h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleFormSubmit}>
           <div>
             <label
               htmlFor="email"
@@ -26,12 +54,11 @@ const LoginPage = () => {
             </label>
             <div className="mt-2">
               <input
-                id="email"
-                name="email"
-                type="email"
+                value={userName}
+                onChange={handleUserNameChange}
+                type="text"
                 required
-                autoComplete="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -55,12 +82,11 @@ const LoginPage = () => {
             </div>
             <div className="mt-2">
               <input
-                id="password"
-                name="password"
+                value={password}
+                onChange={handlePasswordChange}
                 type="password"
                 required
-                autoComplete="current-password"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -68,16 +94,17 @@ const LoginPage = () => {
           <div>
             <button
               type="submit"
+              disabled={isLoading}
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              {isLoading ? 'Loading...' : 'Sign in'}
             </button>
           </div>
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
           {/* Not a member?{" "} */}
-          {`Don't have an account?`}{" "}
+          {`Don't have an account?`}{' '}
           <Link
             href="/signup"
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 underline"
---
+++ pages/utils/api.ts
@@ -0,0 +1,12 @@
+const API_BASE_URL = 'https://dummyjson.com/auth';
+
+export const authenticateUser = async function (requestBody: any) {
+  const loginData = { username: 'emilys', password: 'emilyspass' };
+  const res = await fetch(`${API_BASE_URL}/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(loginData),
+  });
+  const data = await res.json();
+  return data;
+};
