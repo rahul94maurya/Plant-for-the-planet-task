d0b0a4e2b9a7cb8457efa17734ebb44d0c21dc89
rahul <getrahulmaurya1994@gmail.com>
2024-09-16 23:23:19 +0530

    sign up button is making API call

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,3 @@
 export const API_BASE_URL = 'https://dummyjson.com/auth';
+
+export const genders=[{label:"Male",value:"Male"},{label:"Female",value:"Female"},{label:"Prefer not to say",value:"Prefer not to say"}]
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,26 +1,81 @@
-import Link from "next/link";
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-
-import "react-datepicker/dist/react-datepicker.css";
+import { genders } from '@/lib/constants';
+import { authenticateUser } from '@/services/api';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useRouter } from 'next/router';
 
 const SignupPage = () => {
-  const [startDate, setStartDate] = useState<Date>(new Date());
+  const router = useRouter();
+  const [dateOfBirth, setDateOfBirth] = useState<Date | null>(new Date());
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [gender, setGender] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const handleUserNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  };
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setDescription(event.target.value);
+  };
+  const handleGenderChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setGender(event.target.value);
+  };
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleFormSubmit = async function (
+    event: React.FormEvent<HTMLFormElement>
+  ) {
+    event.preventDefault();
+    // TODO: Add form submission logic here
+    const requestBody = {
+      email,
+      userName,
+      name,
+      description,
+      gender,
+      password,
+      dateOfBirth: dateOfBirth?.toISOString(),
+    };
+    setIsLoading(true);
+    const response = await authenticateUser(requestBody);
+    if (response.token) {
+      router.push('/');
+    }
+    setIsLoading(false);
+    console.log('Form submitted:', requestBody);
+  };
+
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        {/* <img
+        <img
           alt="Your Company"
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
           className="mx-auto h-10 w-auto"
-        /> */}
+        />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
+          Sign up for an account
         </h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleFormSubmit}>
           <div>
             <label
               htmlFor="email"
@@ -30,11 +85,10 @@ const SignupPage = () => {
             </label>
             <div className="mt-2">
               <input
-                id="email"
-                name="email"
+                value={email}
+                onChange={handleEmailChange}
                 type="email"
                 required
-                autoComplete="email"
                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -48,11 +102,10 @@ const SignupPage = () => {
             </label>
             <div className="mt-2">
               <input
-                id="email"
-                name="email"
-                type="email"
+                value={userName}
+                onChange={handleUserNameChange}
+                type="text"
                 required
-                autoComplete="email"
                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -76,11 +129,10 @@ const SignupPage = () => {
             </div>
             <div className="mt-2">
               <input
-                id="password"
-                name="password"
+                value={password}
+                onChange={handlePasswordChange}
                 type="password"
                 required
-                autoComplete="current-password"
                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -94,81 +146,66 @@ const SignupPage = () => {
             </label>
             <div className="mt-2">
               <input
-                id="email"
-                name="email"
-                type="email"
+                value={name}
+                onChange={handleNameChange}
+                type="text"
                 required
                 autoComplete="email"
                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Date of Birth
-            </label>
-            <div className="mt-2">
-              <DatePicker
-                name="startDate"
-                id="startDate"
-                selected={startDate}
-                // onChange={(date: Date) => setStartDate(date)}
-                maxDate={new Date()}
-                peekNextMonth
-                showMonthDropdown
-                showYearDropdown
-                closeOnScroll={true}
-                scrollableYearDropdown
-                yearDropdownItemNumber={100}
-                className="w-[17.6rem] min-w-[17.6rem] rounded-[7px] !border !border-extralightgray bg-white p-1.5 py-2.5 text-sm text-cityColor ring-1 ring-transparent placeholder:text-extralightgray focus:!border-extralightgray focus:!border-t-extralightgray focus:ring-extralightgray"
-                // showTimeSelect
-                dateFormat="dd/MM/yyyy"
-                // timeIntervals={15}
-                // filterTime={(time) => filterPassedTime(time, "startDate")}
-                // crossOrigin={undefined}
-                required
-              />
-            </div>
-          </div>
-          <div>
-            {/* <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Gender
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                autoComplete="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div> */}
-            <label
-              htmlFor="country"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Gender
-            </label>
-            <div className="mt-2">
-              <select
-                id="country"
-                name="country"
-                autoComplete="country-name"
-                className="block w-full font-medium rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          <div className="sm:flex items-center justify-between xs:space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
-                <option>United States</option>
-                <option>Canada</option>
-                <option>Mexico</option>
-              </select>
+                Date of Birth
+              </label>
+              <div className="mt-2 w-full">
+                <DatePicker
+                  name="startDate"
+                  id="startDate"
+                  selected={dateOfBirth}
+                  onChange={(date: Date | null) => setDateOfBirth(date)}
+                  maxDate={new Date()}
+                  peekNextMonth
+                  showMonthDropdown
+                  showYearDropdown
+                  closeOnScroll={true}
+                  scrollableYearDropdown
+                  yearDropdownItemNumber={100}
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  dateFormat="dd/MM/yyyy"
+                  required
+                />
+              </div>
+            </div>
+            <div className="sm:w-1/2">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Gender
+              </label>
+              <div className="block w-full mt-2">
+                <select
+                  value={gender}
+                  onChange={handleGenderChange}
+                  className="block w-full font-medium rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option value="">Select your gender</option>
+                  {genders.map((gender) => (
+                    <option key={gender.value} value={gender.value}>
+                      {gender.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
           </div>
+
           <div>
             <label
               htmlFor="about"
@@ -178,11 +215,10 @@ const SignupPage = () => {
             </label>
             <div className="mt-2">
               <textarea
-                id="about"
-                name="about"
+                value={description}
+                onChange={handleDescriptionChange}
                 rows={3}
                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={""}
               />
             </div>
           </div>
@@ -191,7 +227,7 @@ const SignupPage = () => {
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              {isLoading ? 'Loading...' : 'Sign up'}
             </button>
           </div>
         </form>
